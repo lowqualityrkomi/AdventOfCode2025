280fdf7a1d2b396c1ac426ee00c6ad38375e5def
lowqualityrkomi <casoni.mirko@gmail.com>
2025-12-01 15:54:03 +0100

    first day completed :))

---
+++ Code/DayOne.cs
@@ -0,0 +1,60 @@
+﻿namespace Code;
+
+public class DayOne
+{
+    public int PartOne(List<string> rotations)
+    {
+        int position = 50;
+        int counter = 0;
+        
+        rotations.ForEach(rotation =>
+        {
+            var rotateTo = Int32.Parse(rotation.Substring(1));
+            var direction = rotation.Substring(0, 1);
+            var delta = direction == "L" ? -1 : 1;
+            
+            position += rotateTo * delta;
+
+            if (position % 100 == 0)
+            {
+                counter++;
+                position %= 100;
+            }
+            else if (position < 0)
+            {
+                position += 100;
+            }
+        });
+        
+        return counter;
+    }
+
+    public int PartTwo(List<string> rotations)
+    {
+        int position = 50;
+        int counter = 0;
+        
+        rotations.ForEach(rotation =>
+        {
+            var rotateTo = Int32.Parse(rotation.Substring(1));
+            var direction = rotation.Substring(0, 1);
+            var delta = direction == "L" ? -1 : 1;
+
+            for (int i = 0; i < rotateTo; i++)
+            {
+                position += delta;
+                if (position % 100 == 0)
+                {
+                    counter++;
+                    position %= 100;
+                }
+                if (position < 0)
+                {
+                    position += 100;
+                }
+            }
+        });
+        
+        return counter;
+    }
+}
---
+++ Test/DayOneTest.cs
@@ -0,0 +1,32 @@
+﻿using Code;
+
+namespace Test;
+
+public class DayOneTest
+{
+    [Fact]
+    public void DayOneExample_Test()
+    {
+        var adventOfCode = new DayOne();
+
+        List<string> rotations = 
+        [
+            "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"
+        ];
+        
+        Assert.Equal(3, adventOfCode.PartOne(rotations));
+    }
+    
+    [Fact]
+    public void DayOneExamplePartTwo_Test()
+    {
+        var adventOfCode = new DayOne();
+
+        List<string> rotations = 
+        [
+            "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"
+        ];
+        
+        Assert.Equal(6, adventOfCode.PartTwo(rotations));
+    }
+}
