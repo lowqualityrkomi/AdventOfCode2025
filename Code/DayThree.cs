60da80e113b17a01803f12eab5663ffbf06442e0
lowqualityrkomi <casoni.mirko@gmail.com>
2025-12-03 10:34:23 +0100

    Day three completed :))

---
+++ Code/DayThree.cs
@@ -0,0 +1,44 @@
+﻿namespace Code;
+
+public class DayThree
+{
+    public Int128 PartOne(string banks, int digitsToGet)
+    {
+        Int128 result = 0;
+
+        List<string> banksArray = banks.Split('\n').ToList();
+    
+        banksArray.ForEach(bank =>
+        {
+            Int128 sum = 0;
+            int numbersToExclude = digitsToGet - 1;
+            int index = 0;
+        
+            for (int i = 0; i < digitsToGet; i++)
+            {
+                Int128 bigger = 0;
+                int skip = i == 0 ? 0 : 1;
+                
+                for (int j = index + skip; j < bank.Length - numbersToExclude; j++)
+                {
+                    int current = Int32.Parse(bank[j].ToString());
+                    if (current > bigger)
+                    {
+                        bigger = current;
+                        index = j;
+                    }
+                }
+
+                bigger *= (Int128)Math.Pow(10, numbersToExclude);
+
+                sum += bigger;
+            
+                numbersToExclude--;
+            }
+
+            result += sum;
+        });
+    
+        return result;
+    }
+}
---
+++ Test/DayThreeTest.cs
@@ -0,0 +1,26 @@
+﻿using Code;
+
+namespace Test;
+
+public class DayThreeTest
+{
+    [Fact]
+    public void PartOne_Test()
+    {
+        var adventOfCode = new DayThree();
+
+        var banks = "987654321111111\n811111111111119\n234234234234278\n818181911112111";
+        
+        Assert.Equal(357, adventOfCode.PartOne(banks, 2));
+    }
+    
+    [Fact]
+    public void PartTwo_Test()
+    {
+        var adventOfCode = new DayThree();
+
+        var banks = "987654321111111\n811111111111119\n234234234234278\n818181911112111";
+        
+        Assert.Equal(3121910778619, adventOfCode.PartOne(banks, 12));
+    }
+}
